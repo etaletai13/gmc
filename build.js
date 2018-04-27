cf8b98ca139b34bffa91d55980628086dbb3b4f5
etaletai <13@etaletai.com>
2018-04-26 21:17:04 -0700

    working v1.5.0

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 var task = require('child_process').exec;
 
 console.log('Building gmc...');
-task('cd gmc/ && build/env.sh go run build/ci.go install ./cmd/gmc | wc -l', function (err, stdout) {
-  console.log(err ? 'Error.' : stdout)
+task('cd gmc/ && build/env.sh go run build/ci.go install ./cmd/gmc', function (err, stdout) {
+  console.log(err ? 'Error.' : 'Success.')
 });
